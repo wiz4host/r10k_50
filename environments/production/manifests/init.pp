06754dbaad0a107b567436193a7071c65580d75d
Ashish <ashish_26j@hotmail.com>
2017-08-26 01:17:25 -0500

    r10k prod commit 1

--- environments/production/manifests/init.pp
+++ environments/production/manifests/init.pp
@@ -1,6 +1,6 @@
 
 node /^node/{
-   notify { "PRODUCTION Node ${::fqdn} has no role set. Here operating system is:  ${::operatingsystem} .": }
+   notify { "R10K: prodction :: ON Node ${::fqdn} has no role set. Here operating system is:  ${::operatingsystem} .": }
    include firstmod
   }
 

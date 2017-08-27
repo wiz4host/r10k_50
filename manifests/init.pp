ccb7662df7047cfbb148f7611b1d8811e725bbc9
Ashish <ashish_26j@hotmail.com>
2017-08-26 22:07:46 -0500

    only environment (instead of whole puppet folder directory)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 
 node /^node/{
    notify { "R10k::DEVELOPMENT Node ${::fqdn} has no role set. Here operating system is:  ${::operatingsystem} .": }
-   include firstmod
+   #include firstmod
   }
 

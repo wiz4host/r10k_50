4d515175c981aec63db39279244f03e7ea3c415a
Ashish <ashish_26j@hotmail.com>
2017-08-26 22:21:52 -0500

    Prod only environment (instead of whole puppet folder directory)

--- manifests/site.pp
+++
@@ -1,9 +0,0 @@
-node /^node/{
-  if $::role {
-      include "role::${::role}"
-  } else {
-      include profile::base
-      notify { "Node ${::fqdn} has no role set. Here operating system is:  ${::operatingsystem} .": }
-  }
-}
-
